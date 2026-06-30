43b57a3711b2f8ad77404414bf037038d8150cb5
brianmutula69-arch <brianmutula69@gmail.com>
2026-06-30 09:17:26 +0300

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,1 +1,26 @@
+export async function shareLink(product) {
 
+    const url =
+        `${location.origin}${location.pathname}?product=${product.id}`;
+
+    if (navigator.share) {
+
+        navigator.share({
+
+            title: product.name,
+
+            text: "View this product on ST LIGHT",
+
+            url
+
+        });
+
+    } else {
+
+        await navigator.clipboard.writeText(url);
+
+        alert("Link copied.");
+
+    }
+
+}
