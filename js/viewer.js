a715c7a7b044398288fb511d95f2941ef667b592
brianmutula69-arch <brianmutula69@gmail.com>
2026-06-30 09:16:36 +0300

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,1 +1,23 @@
+let current = null;
 
+export function openViewer(product) {
+
+    current = product;
+
+    document.getElementById("viewerImage").src = product.image;
+
+    document.getElementById("viewerTitle").innerText = product.name;
+
+    document
+        .getElementById("imageViewer")
+        .classList.remove("hidden");
+
+}
+
+export function closeViewer() {
+
+    document
+        .getElementById("imageViewer")
+        .classList.add("hidden");
+
+}
