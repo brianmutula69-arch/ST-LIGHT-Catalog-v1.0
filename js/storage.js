ec0466f930891e7cfe57b262387cb903d0f1594e
brianmutula69-arch <brianmutula69@gmail.com>
2026-06-30 09:16:22 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,27 @@
+const KEY = "stlight-favorites";
 
+export function getFavorites() {
+    return JSON.parse(localStorage.getItem(KEY)) || [];
+}
+
+export function saveFavorite(id) {
+
+    let fav = getFavorites();
+
+    if (!fav.includes(id)) {
+
+        fav.push(id);
+
+        localStorage.setItem(KEY, JSON.stringify(fav));
+
+    }
+
+}
+
+export function removeFavorite(id) {
+
+    let fav = getFavorites().filter(x => x !== id);
+
+    localStorage.setItem(KEY, JSON.stringify(fav));
+
+}
